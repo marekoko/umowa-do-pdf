9b827773b1304cbccad139c65dd855cc8242fe48
Marek Oko <marek.m.oko@gmail.com>
2019-03-21 13:23:55 +0100

    Add Test project with xUnit

---
+++ Tests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void File_should_be_generated_properly()
+        {
+            
+        }
+
+    }
+}
