02603a64e95e9e969adbcc06135c860c303fcf3a
Marek Oko <marek.m.oko@gmail.com>
2019-05-13 07:40:10 +0200

    Added Polish grammar stuff, not implemened

---
+++ umowaDoPDF/Noun.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umowaDoPDF
+{
+    class Noun
+    {
+        public class Zloty
+        {
+            public static Dictionary<short, string> ZlotyWithNumeralsDict = new Dictionary<short, string>
+            {
+                {1, "złoty" },
+                {234, "złote" },
+                {567, "złotych" }
+            };
+            
+
+            public static string ZlotyOnlyOne { get; set; } = "złoty";
+            public static string Zloty234 { get; set; } = "złote";
+            public static string Zloty567 { get; set; } = "złotych";
+        }
+        public class Thousand
+        {
+            public static string ThousandOnlyOne { get; set; } = "tysiąc";
+            public static string Thousand234 { get; set; } = "tysiące";
+            public static string Thousand567 { get; set; } = "tysięcy";
+        }
+    }
+
+}
---
+++ umowaDoPDF/PolishGrammar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umowaDoPDF
+{
+    class PolishGrammar
+    {
+        public static string NounsWithNumeralsVariety(string Number, Dictionary<short, string> NounsWithNumeralsDict)
+        {
+            string NounCorrectForm = "";
+            int intNumber = Convert.ToInt32(Number);
+
+            List<int> ListFor1 = new List<int>() { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            List<int> ListFor567 = new List<int>() { 0, 1, 5, 6, 7, 8, 9 };
+            List<int> ListFor234 = new List<int>() { 2, 3, 4 };
+
+            if (intNumber == 1 && Number.Trim().Length == 1)
+            {
+                NounCorrectForm = NounsWithNumeralsDict[1];
+            }
+            else if (ListFor1.Any(x => x == intNumber) || ListFor567.Any(x => x == intNumber % 10))
+            {
+                NounCorrectForm = NounsWithNumeralsDict[567];
+            }
+            else if (ListFor234.Any(x => x == intNumber % 10))
+            {
+                NounCorrectForm = NounsWithNumeralsDict[234];
+            }
+            return NounCorrectForm;
+        }
+    }
+}
