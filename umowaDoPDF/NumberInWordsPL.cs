9853d76466ffcc2e75510582f232c751f531b0d6
Marek Oko <marek.m.oko@gmail.com>
2019-05-18 21:04:38 +0200

    *Bug Fixed: todo: !!!! BUG in numbers extra whitespace in hundreds between name of each hundred and once digit( like 309, 308, 407) *Clearing up comments and todos

--- umowaDoPDF/NumberInWordsPL.cs
+++ umowaDoPDF/NumberInWordsPL.cs
@@ -8,9 +8,6 @@ namespace umowaDoPDF
 {
     class NumberInWordsPL
     {
-        // 
-        // todo: !!!! BUG in numbers extra whitespace in hundreds between name of each hundred and once digit( like 309, 308, 407)
-        
         public static string ConvertNumberToWordsPL(string Number)
         {
             string word = "";
@@ -100,54 +97,12 @@ namespace umowaDoPDF
             //Console.WriteLine("asd" + "\"" + word + "\"" + "asdasd");
             return word.Trim();
         }
-
-        //public static string ZlotyVariety(string Number)
-        //{
-        //    /*
-        //     * Poniższa zasada działa w przypadku odmiany wszystkich rzeczowników przy licznikach.
-        //    Pierwszy liczebnik to jedyny wyjątek przy liczbie "1" i słowie "złoty".
-        //    Dla liczb z zakresu 5–14 lub gdy ostatnia cyfra liczby wynosi 1, 5, 6, 7, 8, 9, 0 
-        //    mówi i pisze się „złotych” (np. 18 złotych, 85 złotych). Te liczby łączą się z dopełniaczem. 
-        //    Ostatnia cyfra 2, 3, 4 – mówi i pisze się „złote” (np. 42 złote, 104 złote). Liczby te z kolei 
-        //    podaje się w formie mianownika.
-        //     TRANSLATED TO ENGLISH:
-        //     The following rule works in the case of a variety of all nouns at counters(numerals).
-        //    The first numeral is the only exception to the number "1" and the word "złoty".
-        //    For numbers in the range 5-14 or when the last digit of the number is 1, 5, 6, 7, 8, 9, 0
-        //    says and writes „złotych”(eg 18 złotych, 85 złotych).These numbers connect to the complement.
-        //    The last digit 2, 3, 4 - says and "gold" is written(eg 42 złote, 104 złote). These numbers in turn
-        //    is given in the form of a nominative.
-        //    */
-        //    string ZlotyCorrectForm = "";
-        //    int intNumber = Convert.ToInt32(Number);
-            
-        //    List<int> listZlotych = new List<int>() { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-        //    List<int> listZlotychLastDigit = new List<int>() { 0, 1, 5, 6, 7, 8, 9 };
-        //    List<int> listZloteLastDigit = new List<int>() { 2, 3, 4 };
-
-        //    if (intNumber == 1 && Number.Trim().Length == 1)
-        //    {
-        //        ZlotyCorrectForm = "złoty";
-        //    }
-        //    else if (listZlotych.Any(x => x == intNumber) || listZlotychLastDigit.Any(x => x == intNumber % 10))
-        //    {
-        //        ZlotyCorrectForm = "złotych";
-        //    }
-        //    else if (listZloteLastDigit.Any(x => x == intNumber % 10))
-        //    {
-        //        ZlotyCorrectForm = "złote";
-        //    }
-        //    return ZlotyCorrectForm;
-        //}
-
-
         private static string Ones(string Number)
         {
             int intOnesNumber = Convert.ToInt32(Number);
-            string name = null;
+            string name = "";
             switch (intOnesNumber)
             {
-
                 case 1:
                     name = "jeden";
                     break;
@@ -176,12 +131,12 @@ namespace umowaDoPDF
                     name = "dziewięć";
                     break;
             }
-            return name;
+            return name.Trim();
         }
         private static string Tens(string Number)
         {
             int intTensNumber = Convert.ToInt32(Number);
-            string name = null;
+            string name = "";
             switch (intTensNumber)
             {
                 case 10:
@@ -245,14 +200,14 @@ namespace umowaDoPDF
                     }
                     break;
             }
-            return name;
+            return name.Trim();
         }
         private static string Hundreds(string Number)
         {
             int intHundredsNumber = Convert.ToInt32(Number);
-            Console.WriteLine(Number);
-            Console.WriteLine(intHundredsNumber);
-            //return "sto jeden";
+            //Console.WriteLine(Number);
+            //Console.WriteLine(intHundredsNumber);
+
             string name = "";
             switch (intHundredsNumber)
             {
@@ -290,7 +245,7 @@ namespace umowaDoPDF
                     }
                     break;
             }
-            return name;
+            return name.Trim();
         }
     }
 }
