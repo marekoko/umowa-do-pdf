93015e9c79a38d763139051990d0bafbba71e240
Marek Oko <marek.m.oko@gmail.com>
2019-05-18 23:31:10 +0200

    *Nouns encapsulation: getters setters

--- umowaDoPDF/Noun.cs
+++ umowaDoPDF/Noun.cs
@@ -10,31 +10,36 @@ namespace umowaDoPDF
     {
         public class Zloty
         {
-            public static Dictionary<short, string> ZlotyWithNumeralsDict = new Dictionary<short, string>
+            private static Dictionary<short, string> zlotyWithNumeralsDict = new Dictionary<short, string>
             {
                 {1, "złoty" },
                 {234, "złote" },
                 {567, "złotych" }
             };
+
+            public static Dictionary<short, string> ZlotyWithNumeralsDict { get => zlotyWithNumeralsDict; set => zlotyWithNumeralsDict = value; }
         }
         public class Thousand
         {
-            public static Dictionary<short, string> ThousandWithNumeralsDict = new Dictionary<short, string>
+            private static Dictionary<short, string> thousandWithNumeralsDict = new Dictionary<short, string>
             {
                 {1, "tysiąc" },
                 {234, "tysiące" },
                 {567, "tysięcy" }
             };
 
-        }public class Million
+            public static Dictionary<short, string> ThousandWithNumeralsDict { get => thousandWithNumeralsDict; set => thousandWithNumeralsDict = value; }
+        }
+        public class Million
         {
-            public static Dictionary<short, string> MillionWithNumeralsDict = new Dictionary<short, string>
+            private static Dictionary<short, string> millionWithNumeralsDict = new Dictionary<short, string>
             {
                 {1, "milion" },
                 {234, "miliony" },
                 {567, "milionów" }
             };
 
+            public static Dictionary<short, string> MillionWithNumeralsDict { get => millionWithNumeralsDict; set => millionWithNumeralsDict = value; }
         }
     }
 
